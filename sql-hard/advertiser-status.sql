6a883a756288d7538e2180d8d1018b5af8ab4909
jdegand <70610011+jdegand@users.noreply.github.com>
2025-11-19 19:27:28 -0500

    fix: advertiser-status

--- sql-hard/advertiser-status.sql
+++ sql-hard/advertiser-status.sql
@@ -1,45 +1,3 @@
-/*
--- Need LEFT JOIN to get null values
--- but this isn't enough as you miss payers present in advertiser
--- and not found in daily_pay 
--- fitdata is missing
--- need a case to look for NULL as well? Yes.
--- changing to FULL JOIN -> you get all data, but fitdata's new_status is NULL 
-
-SELECT 
-    user_id,
-    CASE
-        WHEN status = 'NEW' AND EXISTS (SELECT 1 FROM daily_pay WHERE daily_pay.user_id = advertiser.user_id) THEN 'EXISTING'
-        WHEN status = 'NEW' THEN 'CHURN'
-        WHEN status = 'EXISTING' AND EXISTS (SELECT 1 FROM daily_pay WHERE daily_pay.user_id = advertiser.user_id) THEN 'EXISTING'
-        WHEN status = 'EXISTING' THEN 'CHURN'
-        WHEN status = 'CHURN' AND EXISTS (SELECT 1 FROM daily_pay WHERE daily_pay.user_id = advertiser.user_id) THEN 'RESURRECT'
-        WHEN status = 'CHURN' THEN 'CHURN'
-        WHEN status = 'RESURRECT' AND EXISTS (SELECT 1 FROM daily_pay WHERE daily_pay.user_id = advertiser.user_id) THEN 'EXISTING'
-        WHEN status = 'RESURRECT' THEN 'CHURN'
-    END AS new_status
-FROM advertiser LEFT JOIN daily_pay USING(user_id);
-*/
-
-/*
--- change to FULL JOIN and add conditional to check for NULL status 
-
-SELECT 
-    user_id,
-    CASE
-        WHEN status = 'NEW' AND EXISTS (SELECT 1 FROM daily_pay WHERE daily_pay.user_id = advertiser.user_id) THEN 'EXISTING'
-        WHEN status = 'NEW' THEN 'CHURN'
-        WHEN status = 'EXISTING' AND EXISTS (SELECT 1 FROM daily_pay WHERE daily_pay.user_id = advertiser.user_id) THEN 'EXISTING'
-        WHEN status = 'EXISTING' THEN 'CHURN'
-        WHEN status = 'CHURN' AND EXISTS (SELECT 1 FROM daily_pay WHERE daily_pay.user_id = advertiser.user_id) THEN 'RESURRECT'
-        WHEN status = 'CHURN' THEN 'CHURN'
-        WHEN status = 'RESURRECT' AND EXISTS (SELECT 1 FROM daily_pay WHERE daily_pay.user_id = advertiser.user_id) THEN 'EXISTING'
-        WHEN status = 'RESURRECT' THEN 'CHURN'
-        WHEN status IS NULL THEN 'NEW'
-    END AS new_status
-FROM advertiser FULL JOIN daily_pay USING(user_id);
-*/
-
 SELECT 
     user_id, 
     CASE 
@@ -50,4 +8,5 @@ SELECT
     END AS new_status 
 FROM advertiser
 FULL JOIN daily_pay
-USING(user_id);
+USING(user_id)
+ORDER BY user_id;
