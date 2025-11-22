6d6f34d2a8abd2e94dcb97451aaa503380384bc0
jdegand <70610011+jdegand@users.noreply.github.com>
2025-11-21 19:26:40 -0500

    fix: users-third-transaction

--- sql-medium/users-third-transaction.sql
+++ sql-medium/users-third-transaction.sql
@@ -1,19 +1,14 @@
 WITH numbered_rows AS (
-    SELECT *, ROW_NUMBER() OVER (ORDER BY user_id) AS row_num
+    SELECT 
+        user_id,
+        spend,
+        transaction_date,
+        ROW_NUMBER() OVER (
+            PARTITION BY user_id 
+            ORDER BY transaction_date
+        ) AS row_num
     FROM transactions
 )
 SELECT user_id, spend, transaction_date
 FROM numbered_rows
-WHERE row_num % 3 = 0;
-
-/*
-WITH numbered_rows AS (
-    SELECT *, ROW_NUMBER() OVER (ORDER BY user_id) AS row_num
-    FROM transactions
-)
-SELECT * 
-FROM numbered_rows
-WHERE row_num % 3 = 0;
-
--- if you use SELECT *, you can see the row numbers as well
-*/
+WHERE row_num = 3;
