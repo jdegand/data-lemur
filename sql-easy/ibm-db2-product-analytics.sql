4de80dcb53afa59f69c87a74cc849a319da918cf
jdegand <70610011+jdegand@users.noreply.github.com>
2025-11-21 19:13:58 -0500

    fix: ibm-db2-product-analytics

--- sql-easy/ibm-db2-product-analytics.sql
+++ sql-easy/ibm-db2-product-analytics.sql
@@ -4,15 +4,12 @@ SELECT
 FROM (
     SELECT
         employees.employee_id,
-        COALESCE(COUNT(DISTINCT queries.query_id), 0) AS unique_queries
+        COUNT(DISTINCT queries.query_id) AS unique_queries
     FROM employees
     LEFT JOIN queries ON employees.employee_id = queries.employee_id
         AND queries.query_starttime >= '2023-07-01'
-        AND queries.query_starttime <= '2023-09-30'
+        AND queries.query_starttime <= '2023-10-01'
     GROUP BY employees.employee_id
 ) AS sub
 GROUP BY sub.unique_queries
-ORDER BY sub.unique_queries ASC;
-
--- need LEFT JOIN to get the 0 values
--- I think you can use dates either way -> 07/01/2023 or 2023-07-01
+ORDER BY sub.unique_queries;
