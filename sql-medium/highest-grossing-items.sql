b3d63d39aaa0a0a930cda7d577e6e472488dff11
jdegand <70610011+jdegand@users.noreply.github.com>
2025-11-21 19:56:12 -0500

    fix: highest-grossing-items

--- sql-medium/highest-grossing-items.sql
+++ sql-medium/highest-grossing-items.sql
@@ -1,19 +1,24 @@
-WITH RankedProducts AS (
+WITH ProductTotals AS (
     SELECT
         category,
         product,
-        spend,
-        ROW_NUMBER() OVER (PARTITION BY category ORDER BY spend DESC) AS rn
-    FROM
-        product_spend
-    WHERE
-        EXTRACT(YEAR FROM transaction_date) = 2022
+        SUM(spend) AS total_spend
+    FROM product_spend
+    WHERE EXTRACT(YEAR FROM transaction_date) = 2022
+    GROUP BY category, product
+),
+RankedProducts AS (
+    SELECT
+        category,
+        product,
+        total_spend,
+        ROW_NUMBER() OVER (PARTITION BY category ORDER BY total_spend DESC) AS rn
+    FROM ProductTotals
 )
 SELECT
     category,
     product,
-    spend as total_spend
-FROM
-    RankedProducts
-WHERE
-    rn <= 2;
+    total_spend
+FROM RankedProducts
+WHERE rn <= 2
+ORDER BY category, total_spend DESC;
