11952c2a8b01ff531b39b4914d8fbc601f6106e2
jdegand <70610011+jdegand@users.noreply.github.com>
2025-11-24 18:19:54 -0500

    fix: swapped-food-delivery

--- sql-medium/swapped-food-delivery.sql
+++ sql-medium/swapped-food-delivery.sql
@@ -1,25 +1,15 @@
 WITH CorrectedData AS (
     SELECT 
         CASE 
-            WHEN MOD(order_id, 2) = 1 AND LEAD(item) OVER (ORDER BY order_id) IS NULL THEN order_id + 1
-            ELSE LEAD(order_id) OVER (ORDER BY order_id)
-        END AS corrected_order_id,
-        item
+            WHEN MOD(order_id, 2) = 1 
+                THEN COALESCE(LEAD(item) OVER (ORDER BY order_id), item)
+            ELSE COALESCE(LAG(item) OVER (ORDER BY order_id), item)
+        END AS corrected_item,
+        order_id
     FROM orders
 )
-SELECT corrected_order_id - 1, item FROM CorrectedData;
-
-/*
-WITH CorrectedData AS (
-    SELECT 
-        CASE 
-            WHEN MOD(order_id, 2) = 1 AND LEAD(item) OVER (ORDER BY order_id) IS NULL THEN order_id + 1
-            ELSE LEAD(order_id) OVER (ORDER BY order_id)
-        END AS corrected_order_id,
-        item
-    FROM orders
-)
-SELECT corrected_order_id - 1 AS corrected_order_id, item
+SELECT order_id AS corrected_order_id, corrected_item AS item
 FROM CorrectedData
 ORDER BY corrected_order_id;
-*/
+
+-- need coalesce to handle null on last item of LEAD
