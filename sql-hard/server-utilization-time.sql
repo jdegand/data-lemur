f11c02dbee66dc03f341c699bbfe5c2a7fb28a63
jdegand <70610011+jdegand@users.noreply.github.com>
2025-11-24 18:23:11 -0500

    fix: server-util-time

--- sql-hard/server-utilization-time.sql
+++ sql-hard/server-utilization-time.sql
@@ -1,72 +1,18 @@
--- can't use DATEDIFF
--- This is probably wrong - but it seems close
--- changing from ROW_NUMBER to RANK or DENSE_RANK produced same result
--- PARTITION clauses problematic?
-
 WITH cte AS (
-  SELECT *,
-         ROW_NUMBER() OVER (PARTITION BY server_id, session_status ORDER BY status_time) AS row
+  SELECT 
+      server_id,
+      session_status,
+      status_time,
+      LEAD(status_time) OVER (
+          PARTITION BY server_id 
+          ORDER BY status_time
+      ) AS next_time,
+      LEAD(session_status) OVER (
+          PARTITION BY server_id 
+          ORDER BY status_time
+      ) AS next_status
   FROM server_utilization
-),
-cte2 AS (
-  SELECT server_id,
-         session_status,
-         status_time AS start_time,
-         LEAD(status_time) OVER (PARTITION BY server_id, row ORDER BY row) AS stop_time
-  FROM cte
 )
-SELECT SUM(EXTRACT(DAY FROM stop_time - start_time)) AS total_uptime_days
-FROM cte2;
-
-/*
-WITH cte AS (
-  SELECT *,
-         ROW_NUMBER() OVER (PARTITION BY server_id, session_status ORDER BY status_time) AS row
-  FROM server_utilization
-),
-cte2 AS (
-  SELECT server_id,
-         session_status,
-         status_time AS start_time,
-         LEAD(status_time) OVER (PARTITION BY server_id, row ORDER BY row) AS stop_time
-  FROM cte
-)
-SELECT SUM(DATE_PART('day', stop_time - start_time)) AS total_uptime_days
-FROM cte2;
-*/
-
-/*
--- result doesn't change with CAST added
-
-WITH cte AS (
-    SELECT *, 
-           ROW_NUMBER() OVER (PARTITION BY server_id, session_status ORDER BY status_time) AS row 
-    FROM server_utilization
-), 
-cte2 AS (
-    SELECT server_id, 
-           session_status, 
-           status_time AS start_time, 
-           LEAD(status_time) OVER (PARTITION BY server_id, row ORDER BY row) AS stop_time 
-    FROM cte
-) 
-SELECT SUM(CAST(DATE_PART('day', stop_time - start_time) AS INTEGER)) AS total_uptime_days 
-FROM cte2;
-*/
-
-/*
-WITH cte AS (
-  SELECT *,
-         ROW_NUMBER() OVER (PARTITION BY session_status ORDER BY status_time) AS row
-  FROM server_utilization
-),
-cte2 AS (
-  SELECT server_id,
-         session_status,
-         status_time AS start_time,
-         LEAD(status_time) OVER (PARTITION BY row ORDER BY row) AS stop_time
-  FROM cte
-)
-SELECT SUM(EXTRACT(DAY FROM stop_time - start_time)) AS total_uptime_days
-FROM cte2;
-*/
+SELECT ROUND(SUM(EXTRACT(EPOCH FROM next_time - status_time) / 86400), 0) AS total_uptime_days
+FROM cte
+WHERE session_status = 'start' AND next_status = 'stop';
