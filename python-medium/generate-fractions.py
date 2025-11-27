c2f418d0d10fbe14e9110f3f64f6d58ef75d2fa4
jdegand <70610011+jdegand@users.noreply.github.com>
2025-11-26 22:18:32 -0500

    feat: add generate-fractions

---
+++ python-medium/generate-fractions.py
@@ -0,0 +1,9 @@
+import math
+
+def generate_fractions(n):
+    result = []
+    for numerator in range(1, n):
+        for denominator in range(numerator + 1, n + 1):
+            if math.gcd(numerator, denominator) == 1:
+                result.append([numerator, denominator])
+    return result
