306f62e22917bea5315f3a66270095a6b208e03a
jdegand <70610011+jdegand@users.noreply.github.com>
2025-11-28 18:09:00 -0500

    feat: two-sum-part-two

---
+++ python-medium/two-sum-part-two.py
@@ -0,0 +1,15 @@
+def two_sum(nums: list[int], target: int) -> list[int]:
+    # Two pointers required for constant time
+    left, right = 0, len(nums) - 1
+    
+    while left < right:
+        current_sum = nums[left] + nums[right]
+        
+        if current_sum == target:
+            return [left, right]
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+    
+    return [-1, -1]
