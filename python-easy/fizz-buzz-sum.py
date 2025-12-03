ab9b018d05b524a5d0c6783cddf96c49db62c21e
jdegand <70610011+jdegand@users.noreply.github.com>
2025-12-03 13:46:43 -0500

    fix: replace sum var with total

--- python-easy/fizz-buzz-sum.py
+++ python-easy/fizz-buzz-sum.py
@@ -1,11 +1,11 @@
 def fizz_buzz_sum(target):
-    sum = 0
+    total = 0
     num = target
     
     while num > 0:
         num -= 1
         
         if num % 5 == 0 or num % 3 == 0:
-            sum += num
+            total += num
     
-    return sum
+    return total
